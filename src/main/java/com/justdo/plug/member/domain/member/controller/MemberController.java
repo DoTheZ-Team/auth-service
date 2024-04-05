2388e121833214735c7c0e8bc42ee8d2214fd559
준희 <ht0729@gachon.ac.kr>
2024-04-05 09:28:26 +0900

    PLUG-137 Refactor : 엔드포인트 변경

--- src/main/java/com/justdo/plug/member/domain/member/controller/MemberController.java
+++ src/main/java/com/justdo/plug/member/domain/member/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member/info")
+@RequestMapping("/members/**")
 @Slf4j
 
 public class MemberController {
