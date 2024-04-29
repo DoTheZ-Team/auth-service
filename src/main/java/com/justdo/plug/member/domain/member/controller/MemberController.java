52a695443d1c920054f14b69db93176a2f4f1460
gyeom <rlagusrua3687@gmail.com>
2024-04-29 15:34:09 +0900

    GLUE-249 feat: Swagger & Eureka 적용

--- src/main/java/com/justdo/plug/member/domain/member/controller/MemberController.java
+++ src/main/java/com/justdo/plug/member/domain/member/controller/MemberController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/members")
+@RequestMapping("/auths")
 @Slf4j
 
 public class MemberController {
