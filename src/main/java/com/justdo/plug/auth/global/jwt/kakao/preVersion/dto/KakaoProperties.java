662804892ab405dc936b384a40e1a159e47a2952
준희 <ht0729@gachon.ac.kr>
2024-05-14 01:13:14 +0900

    GLUE-100 Feat: 이전 버전 카카오 로그인 api 복구

---
+++ src/main/java/com/justdo/plug/auth/global/jwt/kakao/preVersion/dto/KakaoProperties.java
@@ -0,0 +1,10 @@
+package com.justdo.plug.auth.global.jwt.kakao.preVersion.dto;
+
+import lombok.Getter;
+
+@Getter
+public class KakaoProperties {
+    private String nickname;
+    private String profile_image;
+    private String thumbnail_image;
+}
